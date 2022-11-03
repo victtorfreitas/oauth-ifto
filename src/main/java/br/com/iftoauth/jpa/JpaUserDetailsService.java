07c255065feb0c02b9bf52d4cb01010aa47c7d4b
victtor <victtorfreitas95@gmail.com>
2022-11-03 14:50:53 -0300

    Add permissions at token scope

--- src/main/java/br/com/iftoauth/jpa/JpaUserDetailsService.java
+++ src/main/java/br/com/iftoauth/jpa/JpaUserDetailsService.java
@@ -1,7 +1,12 @@
 package br.com.iftoauth.jpa;
 
+import br.com.iftoauth.jpa.entity.UserEntity;
 import br.com.iftoauth.jpa.repository.UserRepository;
+import java.util.Collection;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -17,7 +22,14 @@ public class JpaUserDetailsService implements UserDetailsService {
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
     final var userEntity = userRepository.findByEmail(username)
         .orElseThrow(() -> new UsernameNotFoundException(getMsg(username)));
-    return new AuthUser(userEntity);
+    return new AuthUser(userEntity, getAuthorities(userEntity));
+  }
+
+  private Collection<? extends GrantedAuthority> getAuthorities(UserEntity userEntity) {
+    return userEntity.getGroups()
+        .stream()
+        .map(groupUserEntity -> new SimpleGrantedAuthority(groupUserEntity.getName().toUpperCase()))
+        .collect(Collectors.toSet());
   }
 
   private static String getMsg(String username) {
